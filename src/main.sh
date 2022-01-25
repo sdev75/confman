fca7f2b699d1a00cc12a909ba37c8be198681175
sdev75 <29279974+sdev75@users.noreply.github.com>
2022-01-25 17:49:12 +0100

    still some hiccups to fix

--- src/main.sh
+++ src/main.sh
@@ -2,7 +2,18 @@
 
 #include misc.sh
 
-CACHEDIR="~/.cache/confman"
+# Set default cachedir 
+# default value to ~/.cache/confman
+# Its possible to pass the value by ENV using CACHEDIR=. confman
+if [ -z ${CACHEDIR+x} ]; then
+  CACHEDIR="~/.cache/confman"
+fi
+
+CACHEDIR=$(realpath "$CACHEDIR")
+if [ "$CACHEDIR" = "$(pwd)" ]; then
+  errmsg "In sourcing disabled"
+  exit 1
+fi
 if [ ! -d $CACHEDIR ]; then
   read -rep "$CACHEDIR does not exist. Would you like to create it? (y/n)" -n 1
   if echo $REPLY | grep -Eq '[yY]'; then
@@ -11,6 +22,14 @@ if [ ! -d $CACHEDIR ]; then
    errmsg "$CACHEDIR is required to use this program" 
   fi
 fi
+
+snapshot_id(){
+  #echo $(date +%Y-%d-%b) | tr '[:upper:]' '[:lower:]'
+  echo $(date +%s)
+}
+
+x=$(snapshot_id)
+echo "x is $x"
 
 checksum(){
   local res=$(sha256sum "$1" | awk 'print $1}')
@@ -37,6 +56,40 @@ parse(){
     errmsg ".confman not found in '$(pwd)'."
     exit 1
   fi
+  echo CACHEDIR is $CACHEDIR
+  script=('
+    BEGIN {
+      filename = cachedir "/{groupid}_" snapshot_id ".tar.gz"
+      postfix = "_" snapshot_id ".tar.gz"
+    }
+    {
+      if (length(groupid) > 0){
+        print "GZIP ALLOWED TO BE PERFORMED HERE"
+      }
+
+      if ($0 ~ /[[:alnum:]]+ *{/) { 
+        match($0,/^([a-z]+).*/,m)
+        groupid = m[1]
+        gensub(/^([a-z]+).*/,"__\\1(){", "g")
+        print "groupid is " groupid
+      } else {
+        print "using groupid = " groupid
+        if ($1 == "add") {
+          if (substr($2,length($2),1) == "/"){
+            print "tar -r --file=" filename " " $2
+            print "ADD COMMAND WITH DIRECTORY " $2
+          }
+          a = "gzip allowed"
+        }
+      }
+
+    }
+  ')
+
+  awk \
+    -v cachedir="$CACHEDIR" \
+    -v snapshot_id="$(snapshot_id)" \
+    "$script" .confman
 }
 
 parse
